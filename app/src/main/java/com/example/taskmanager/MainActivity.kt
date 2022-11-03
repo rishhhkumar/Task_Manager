87875f182fd3606f6c4588878b41e564c026e2d1
rishhhkumar <kumarrr@rknec.edu>
2022-11-03 19:53:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.taskmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.Alignment
+import com.example.taskmanager.ui.theme.TaskManagerTheme
+
+
+class TaskManager : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TaskManagerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Task(painterResource(R.drawable.taskmanagerimage),stringResource(R.string.Alert),stringResource(R.string.Comment))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Task(image: Painter, alert: String, comment: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        Text(
+            text = alert,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 24.dp, bottom = 8.dp),
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text=comment,
+            fontSize = 16.sp,
+            modifier = Modifier
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+    }
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun Manager() {
+    TaskManagerTheme{
+        Task(image = painterResource(R.drawable.taskmanagerimage),alert = stringResource(R.string.Alert), comment = stringResource(R.string.Comment))
+    }
+}
